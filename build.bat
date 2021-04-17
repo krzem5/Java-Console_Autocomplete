7737f0fca37f6896ab6084fefa4c14652805a540
Krzem <krzem5.dev@gmail.com>
2021-04-17 13:35:02 +0200

    Push Update 04/17/2021, 13:34:54

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 @echo off
-echo NUL>_.class&&del /s /f /q *.class
 cls
-javac -encoding utf8 -cp com/krzem/console_autocomplete/modules/jna-5.5.0.jar;com/krzem/console_autocomplete/modules/jna-platform-5.5.0.jar; com/krzem/console_autocomplete/Main.java&&java -Dfile.encoding=UTF8 -cp com/krzem/console_autocomplete/modules/jna-5.5.0.jar;com/krzem/console_autocomplete/modules/jna-platform-5.5.0.jar; com/krzem/console_autocomplete/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+python build.py %*
